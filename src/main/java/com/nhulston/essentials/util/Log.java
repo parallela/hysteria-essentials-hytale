b1c109dca92770f8249e5f3fae0bff85d745d918
Nicholas Hulston <nicholashulston@gmail.com>
2026-01-15 12:05:10 -0500

    make essentials logs have colors

--- src/main/java/com/nhulston/essentials/util/Log.java
+++ src/main/java/com/nhulston/essentials/util/Log.java
@@ -8,6 +8,12 @@ import java.util.logging.Level;
 public final class Log {
     private static HytaleLogger logger;
 
+    // ANSI color codes
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String RED = "\u001B[31m";
+    private static final String RESET = "\u001B[0m";
+
     private Log() {}
 
     public static void init(@Nonnull HytaleLogger logger) {
@@ -15,18 +21,18 @@ public final class Log {
     }
 
     public static void info(@Nonnull String message) {
-        logger.at(Level.INFO).log(message);
+        logger.at(Level.INFO).log(GREEN + message + RESET);
     }
 
     public static void warning(@Nonnull String message) {
-        logger.at(Level.WARNING).log(message);
+        logger.at(Level.WARNING).log(YELLOW + message + RESET);
     }
 
     public static void error(@Nonnull String message) {
-        logger.at(Level.SEVERE).log(message);
+        logger.at(Level.SEVERE).log(RED + message + RESET);
     }
 
     public static void error(@Nonnull String message, @Nonnull Throwable throwable) {
-        logger.at(Level.SEVERE).withCause(throwable).log(message);
+        logger.at(Level.SEVERE).withCause(throwable).log(RED + message + RESET);
     }
 }
